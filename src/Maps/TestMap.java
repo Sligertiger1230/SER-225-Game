c5404fbeccc3718b5ab14b642bd1dbb6a6cc3e21
Sligertiger1230 <rjsliger@quinnipiac.edu>
2023-09-27 23:23:43 -0400

    Teleport helping function with bug fixed

--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -70,7 +70,9 @@ public class TestMap extends Map {
 
         getMapTile(2, 6).setInteractScript(new TreeScript());
 
-        getMapTile(18, 13).setInteractScript(new TeleportScript());
+        getMapTile(18, 13).setInteractScript(new TeleportScript(31, 16));
+
+        getMapTile(31, 12).setInteractScript(new TeleportScript(18, 14));
     }
 }
 
--- src/Scripts/TestMap/TeleportScript.java
+++ src/Scripts/TestMap/TeleportScript.java
@@ -5,9 +5,21 @@ import Level.ScriptState;
 
 public class TeleportScript extends Script {
 
+    private float x = 0;
+    private float y = 0;
+    private boolean hasScaled = false;
+
+    public TeleportScript(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
     @Override
     protected void setup() {
         lockPlayer();
+        showTextbox();
+
     }
 
     @Override
@@ -18,9 +30,13 @@ public class TeleportScript extends Script {
 
     @Override
     protected ScriptState execute() {
-        player.setX(1464);
-        player.setY(520);
-        player.moveDown(100);
+        if (!hasScaled) {
+            x = x * 47.35f;
+            y = y * 47.35f;
+            hasScaled = true;
+        }
+
+        player.setLocation(x, y); // Teleport the player
         return ScriptState.COMPLETED;
     }
 }
