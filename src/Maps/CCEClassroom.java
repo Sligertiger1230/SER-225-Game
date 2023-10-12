f35aaadd5fbd5616230b819eee2e0da698c0f833
jubck <julialbock@gmail.com>
2023-10-11 20:18:28 -0400

    fixed tp bug where player was under the map

--- src/Maps/CCEClassroom.java
+++ src/Maps/CCEClassroom.java
@@ -7,6 +7,6 @@ import Utils.Point;
 public class CCEClassroom extends Map{
     public CCEClassroom() {
         super("CCEClassroom.txt", new CommonTileset());
-        this.playerStartPosition = getMapTile(1, 2).getLocation();
+        this.playerStartPosition = getMapTile(1, 4).getLocation();
     }
 }
