3409b6b555b4df271bc3db49b5bf60144fdf63c5
jubck <julialbock@gmail.com>
2023-11-09 01:25:35 -0500

    finished orientation room and first part of cutscene

--- src/Level/Portrait.java
+++ src/Level/Portrait.java
@@ -66,7 +66,7 @@ public class Portrait extends Screen {
         image = ImageLoader.load(imageName);
         spriteHeight = image.getHeight();
         spriteWidth = image.getWidth();
-        this.scale = scale;
+        this.scale = 1;        
         this.rightSide = rightSide;
     }
 
