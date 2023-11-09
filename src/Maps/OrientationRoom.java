b9c22a13ac149d020bdc1ca7da1500741287ec13
jubck <julialbock@gmail.com>
2023-11-09 00:10:00 -0500

    orientation room finish

--- src/Maps/OrientationRoom.java
+++ src/Maps/OrientationRoom.java
@@ -8,6 +8,7 @@ import Level.Trigger;
 import NPCs.Judy;
 import Scripts.CCEClassroom.ChangeMapScript;
 import Scripts.OrientationRoom.JudyScript;
+import Scripts.TestMap.LostBallScript;
 import Tilesets.CommonTileset;
 import Utils.Point;
 
@@ -40,7 +41,20 @@ public class OrientationRoom extends Map{
 
         getMapTile(4, 2).setInteractScript(new ChangeMapScript(1));
 
+        getMapTile(31, 2).setInteractScript(new ChangeMapScript(1));
+
+        getMapTile(30, 2).setInteractScript(new ChangeMapScript(1));
+
+        getMapTile(29, 2).setInteractScript(new ChangeMapScript(1));
+
+        getMapTile(28, 2).setInteractScript(new ChangeMapScript(1));
 
      }
+     @Override
+    public ArrayList<Trigger> loadTriggers() {
+        ArrayList<Trigger> triggers = new ArrayList<>();
+        triggers.add(new Trigger(12, 20, 0, 0, new JudyScript(), "hasStartedGame"));
+        return triggers;
+    }
     
 }
